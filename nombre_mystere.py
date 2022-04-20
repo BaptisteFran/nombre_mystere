1d92b8c7d0874a08d89a4b39e2c80f06d8aa91cc
Baptiste <baptiste.franel@gmail.com>
2022-04-20 15:03:11 +0200

    Update nombre_mystere.py
    - Vérification de l'input utilisateur

--- nombre_mystere.py
+++ nombre_mystere.py
@@ -2,13 +2,18 @@ import random
 
 nombre_mystere = random.randint(1,50)
 essai = input("Devinez le nombre mystère : ")
+if(essai.isdigit()):
+    print("Vous n'avez pas rentré un nombre !")
+    exit() 
+else:
+    essai = int(essai)
 
 # A rajouter :
 # Nombre maximum d'essai avant gameover
 
-if(int(essai) < nombre_mystere):
+if(essai < nombre_mystere):
     resultat = "Le nombre mystère est plus grand !"
-elif(int(essai) > nombre_mystere):
+elif(essai > nombre_mystere):
     resultat = "Le nombre mystère est plus petit !"
 else:
     resultat = "Bravo, vous avez trouvé le nombre mystère qui était : " + str(nombre_mystere)
