bcb22003d67f391a2edbfb0cf8c7242de1f4f0ce
Baptiste <baptiste.franel@gmail.com>
2022-04-20 11:03:36 +0200

    Update nombre_mystere.py
    - Ajout commentaire pour futur fonction

--- nombre_mystere.py
+++ nombre_mystere.py
@@ -3,11 +3,15 @@ import random
 nombre_mystere = random.randint(1,50)
 essai = input("Devinez le nombre mystère : ")
 
+# A rajouter :
+# Nombre maximum d'essai avant gameover
+
 if(int(essai) < nombre_mystere):
     resultat = "Le nombre mystère est plus grand !"
 elif(int(essai) > nombre_mystere):
     resultat = "Le nombre mystère est plus petit !"
 else:
     resultat = "Bravo, vous avez trouvé le nombre mystère qui était : " + str(nombre_mystere)
+
 
 print(resultat)
